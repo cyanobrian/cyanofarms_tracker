1aeb6a5b73208e35f61a4cf3073a90f0c127fffd
Brian Zhang <bszhang@umass.edu>
2026-08-23 21:07:06 -0400

    Fixed error with timezone comparison

--- test.py
+++ test.py
@@ -3,20 +3,22 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame({
-    'Position': range(1, 10001),  # Example range, adjust according to your data
-    'Value': np.random.randn(10000)  # Random data for demonstration
-})
+print(pd.Timestamp.now('US/Eastern').normalize())
+print(pd.offsets.Week(weekday=5).rollback(pd.Timestamp.now('US/Eastern').normalize()))
+# df = pd.DataFrame({
+#     'Position': range(1, 10001),  # Example range, adjust according to your data
+#     'Value': np.random.randn(10000)  # Random data for demonstration
+# })
 
-fig = px.line(df, x='Position', y='Value')
+# fig = px.line(df, x='Position', y='Value')
 
-fig.update_layout(
-    xaxis=dict(
-        rangeslider=dict(
-            visible=True
-        ),
-        type="linear"
-    )
-)
+# fig.update_layout(
+#     xaxis=dict(
+#         rangeslider=dict(
+#             visible=True
+#         ),
+#         type="linear"
+#     )
+# )
 
-st.plotly_chart(fig, use_container_width=True)
+# st.plotly_chart(fig, use_container_width=True)
